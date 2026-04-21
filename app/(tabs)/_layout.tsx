b6a4a1c73cf6e0d476066fe12c5eeffcb74a8a9d
daav1d999 <122431796@umail.ucc.ie>
2026-04-21 21:24:16 +0100

    CRUD operations

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,10 @@
 import { Tabs } from 'expo-router';
-
 export default function TabLayout() {
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
-        options={{ title: 'Students' }}
+        options={{ title: 'Habit Tracker' }}
       />
     </Tabs>
   );
